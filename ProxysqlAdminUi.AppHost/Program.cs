0d1f8423d97f595cc20c5f4c1a6b20b062fbff22
Nikola Sivkov <erasrz@gmail.com>
2024-11-08 00:35:53 +0200

    Removed redundant project files
    I'm not going to use Aspire

--- ProxysqlAdminUi.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.ProxysqlAdminUi_ApiService>("apiservice");
-
-builder.AddProject<Projects.ProxysqlAdminUi_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
-builder.Build().Run();
