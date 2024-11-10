790f398a5898c0bb3514e8eae382333eb4f5ee75
Nikola Sivkov <erasrz@gmail.com>
2024-11-10 17:33:20 +0200

    Added missing view model

---
+++ ProxysqlAdminUi.Web/ViewModel/ServerStatsSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace ProxysqlAdminUi.Web.ViewModel;
+
+public class ServerStatusSummary
+{
+    public int Online { get; set; }
+    public int Offline { get; set; }
+    public int Shunned { get; set; }
+    public int Total => Online + Offline + Shunned;
+}
