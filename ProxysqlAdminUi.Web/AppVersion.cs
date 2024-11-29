7007a1e4a932b081c2e6dc5dc6e95447f2521d46
Nikola Sivkov <erasrz@gmail.com>
2024-11-29 15:54:58 +0200

    Add app version to footer

---
+++ ProxysqlAdminUi.Web/AppVersion.cs
@@ -0,0 +1,6 @@
+﻿namespace ProxysqlAdminUi.Web;
+
+public class AppVersion
+{
+    public string Version = Environment.GetEnvironmentVariable("BUILD_VERSION") ?? DateTime.Now.ToString("yyyy.MM.dd-DEV");
+}
